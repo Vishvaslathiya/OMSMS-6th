ed0219bf007dfc92151bdc1bedb293fd31f0f945
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-04-08 18:28:02 +0530

    Added Sessiona and chnaged UI

--- Customer/Registration_OTP.aspx.cs
+++ Customer/Registration_OTP.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -13,16 +13,8 @@ namespace OMSMS6.Customer
         SqlConnection conn = new SqlConnection("Data Source=LAPTOP-SHON9L4N\\SQLEXPRESS;Initial Catalog=omsms;Integrated Security=True;");
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                ClientScript.RegisterStartupScript(this.GetType(), "ToastrScript", "", true);
-            }
 
-            if (Session["OTP"] != null)
-            {
-                ClientScript.RegisterStartupScript(this.GetType(), "SuccessScript", "toastr.success('OTP sent on your Email Address!')", true);
-            }
-            else
+            if (Session["OTP"] == null)
             {
                 Response.Redirect("Registration.aspx");
             }
@@ -42,19 +34,19 @@ namespace OMSMS6.Customer
                 String password = (String)Session["password"];
                 String gender = (String)Session["gender"];
                 int city = Int32.Parse((String)Session["city"]);
-                String address = (String)Session["address"];
+                //String address = (String)Session["address"];
 
                 conn.Close();
                 conn.Open();
-                SqlCommand insertUser = new SqlCommand("INSERT INTO tblUsers (name, email, password, contact, gender, cityid, address, role, status) VALUES (@name, @email, @password, @contact, @gender, @city, @address, @role, @status);", conn);
+                SqlCommand insertUser = new SqlCommand("INSERT INTO tblUsers (name, email, password, contact, gender, cityid, role, status) VALUES (@name, @email, @password, @contact, @gender, @city, @role, @status);", conn);
                 insertUser.Parameters.AddWithValue("@name", name);
                 insertUser.Parameters.AddWithValue("@email", email);
                 insertUser.Parameters.AddWithValue("@password", password);
                 insertUser.Parameters.AddWithValue("@contact", contact);
                 insertUser.Parameters.AddWithValue("@gender", gender);
                 insertUser.Parameters.AddWithValue("@city", city);
-                insertUser.Parameters.AddWithValue("@address", address);
-                insertUser.Parameters.AddWithValue("@role", "customer");
+                //insertUser.Parameters.AddWithValue("@address", address);
+                insertUser.Parameters.AddWithValue("@role", 1);
                 insertUser.Parameters.AddWithValue("@status", 1);
 
                 int isInserted = insertUser.ExecuteNonQuery();
@@ -66,17 +58,22 @@ namespace OMSMS6.Customer
                     Session["password"] = null;
                     Session["gender"] = null;
                     Session["city"] = null;
-                    Session["address"] = null;
+                    //Session["address"] = null;
                     Session["OTP"] = null;
 
                     conn.Close();
-                    ClientScript.RegisterStartupScript(this.GetType(), "Script", "alert('You have done Successfull Registration!')", true);
-                    Response.Redirect("~/Res/Login.aspx");
+                    Response.Write("<script>alert('Registration done Successfully!'); window.location='../Res/Login.aspx'</script>");
+                    //ClientScript.RegisterStartupScript(this.GetType(), "Script", "alert('You have done Successfull Registration!')", true);
+                    //Response.Redirect("~/Res/Login.aspx");
+                } else
+                {
+                    Response.Write("<script>alert('Something went Wrong!'); window.location='../Customer/Registration.aspx'</script>");
                 }
             }
             else
             {
-                ClientScript.RegisterStartupScript(this.GetType(), "ErrorScript", "toastr.error('OTP is not matched!');", true);
+                Response.Write("<script>alert('OTP is not matched!');</script>");
+                //ClientScript.RegisterStartupScript(this.GetType(), "ErrorScript", "toastr.error('OTP is not matched!');", true);
             }
         }
     }
