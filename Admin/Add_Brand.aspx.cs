555667d6d7c67bf28d3258296e313fae829a0eaf
Vishvas Lathiya <111498112+Vishvaslathiya@users.noreply.github.com>
2024-04-20 23:23:16 +0530

    admin brand added

---
+++ Admin/Add_Brand.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OMSMS6.Admin
+{
+    public partial class Add_Brand : System.Web.UI.Page
+    {
+        //SqlConnection conn = new SqlConnection("Data Source=LAPTOP-SHON9L4N\\SQLEXPRESS;Initial Catalog=omsms;Integrated Security=True;");
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            conn.Close();
+            conn.Open();
+            string name = txtBrand.Text;
+
+            SqlCommand checkBrand = new SqlCommand("SELECT * FROM tblBrand WHERE name = @name", conn);
+            checkBrand.Parameters.AddWithValue("@name", name);
+            SqlDataReader dr = checkBrand.ExecuteReader();
+            if (!dr.Read())
+            {
+                dr.Close();
+                SqlCommand insertBrand = new SqlCommand("INSERT INTO tblBrand (name) VALUES (@name)", conn);
+                insertBrand.Parameters.AddWithValue("@name", name);
+                int isInserted = insertBrand.ExecuteNonQuery();
+                if (isInserted > 0)
+                {
+                    Response.Write("<script>alert('Brand Added Successfully!'); window.location='../Admin/Other.aspx'</script>");
+                }
+                else
+                {
+                    dr.Close();
+                    Response.Write("<script>alert('Error in Brand Addition!');</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Brand Already Exists!');</script>");
+                dr.Close();
+            }
+        }
+    }
+}
