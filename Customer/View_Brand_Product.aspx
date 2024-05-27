7ddd53becfd6e9810b9aaa639d63c08e05f99da0
Vishvas Lathiya <21bmiit127@gmail.com>
2024-05-27 17:13:26 +0530

    View Product Brand Vise

---
+++ Customer/View_Brand_Product.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="OMSMS | Products" Language="C#" MasterPageFile="~/Res/Customer_Navbar.Master" AutoEventWireup="true" CodeBehind="View_Brand_Product.aspx.cs" Inherits="OMSMS6.Customer.View_Brand_Product" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <%-- Tailwind CSS CDN --%>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <%-- Daisy UI CDN --%>
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css" rel="stylesheet"
+        type="text/css" />
+
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <%-- Ionicons Links --%>
+    <script type="module"
+        src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
+
+    <%-- Favicon --%>
+    <link rel="icon" href="Images/logo.png" type="image/png">
+
+    <%-- CSS files --%>
+    <link rel="stylesheet" href="../Res/CSS/Style.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/daisyui@1.10.0"></script>
+
+    <style>
+        .neon-effect {
+            position: relative;
+            display: inline-block;
+        }
+
+            .neon-effect::after {
+                content: "";
+                position: absolute;
+                bottom: -5px;
+                left: 0;
+                width: 100%;
+                height: 2px;
+                background-color: #00ffff;
+                transform: scaleX(0);
+                transition: transform 0.3s ease-in-out;
+            }
+
+            .neon-effect:hover::after {
+                transform: scaleX(1);
+            }
+    </style>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="text-center p-10">
+        <h1 class="font-bold text-white hover:text-[#00ffff] text-4xl neon-effect">Our Products</h1>
+    </div>
+
+    <!-- ✅ Grid Section - Starts Here 👇 -->
+    <form runat="server">
+        <section id="Projects" class="w-fit mx-auto grid xs:grid-cols-1 lg:grid-cols-4 lg:gap-x-5 md:grid-cols-3 md:gap-x-5 sm:grid-cols-2 sm:gap-x-5 justify-items-center justify-center gap-y-20 gap-x-5 mt-10 mb-5">
+            <asp:PlaceHolder ID="ProductContainer" runat="server"></asp:PlaceHolder>
+        </section>
+    </form>
+
+</asp:Content>
---
+++ Customer/View_Brand_Product.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OMSMS6.Customer
+{
+    public partial class View_Brand_Product : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+
+            }
+
+        }
+
+
+        protected void FetchProductsFromDatabase(int bid)
+        {
+            //string connectionString = "Data Source=Vishvas;Initial Catalog=OMSMS;Integrated Security=True;";
+            //string connectionString = "Data Source=LAPTOP-SHON9L4N\\SQLEXPRESS;Initial Catalog=omsms;Integrated Security=True;";
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+
+            using (con)
+            {
+                //SqlCommand cmd = new SqlCommand("SELECT p.Id, p.Name, p.ImageName, pd.Price FROM tblProduct p INNER JOIN tblProductDetail pd ON p.Id = pd.Pid", con);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tblProduct WHERE bid=@bid", con);
+                cmd.Parameters.AddWithValue("@bid", bid);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        //int productId = rdr.GetInt32(0);
+                        //string productName = rdr.GetString(1);
+                        //string imageName = rdr.GetString(2);
+                        //int productPrice = rdr.GetInt32(3);
+                        int productId = int.Parse(rdr["id"].ToString());
+                        string productName = rdr["name"].ToString();
+                        string imageName = rdr["imageName"].ToString();
+                        //int productPrice = int.Parse(rdr["price"].ToString());
+
+                        // Dynamically create HTML elements for product display
+                        //        string productHtml = $@"
+                        //<div class='w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl'>
+                        //    <a href='Cust_View_Product_Details.aspx?id={productId}'>
+                        //        <img src='../Res/Images/{imageName}' alt='Product' class='h-80 w-72 object-cover rounded-t-xl' />
+                        //        <div class='px-4 py-3 w-72'>
+                        //            <p class='text-lg font-bold text-black truncate block capitalize'>{productName}</p>
+                        //            <p class='text-lg font-bold text-black truncate block capitalize'>Price: ₹{productPrice}</p>
+                        //        </div>
+                        //    </a>
+                        //</div>";
+
+                        string productHtml = $@"
+                <div class='w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl'>
+                    <a href='Cust_View_Product_Details.aspx?id={productId}'>
+                        <img src='../Res/Images/{imageName}' alt='Product' class='h-80 w-72 object-cover rounded-t-xl' />
+                        <div class='px-4 py-3 w-72'>
+                            <p class='text-lg font-bold text-black truncate block capitalize'>{productName}</p>
+                        </div>
+                    </a>
+                </div>";
+
+                        // Add the product HTML to the placeholder control or any other container
+                        LiteralControl productControl = new LiteralControl(productHtml);
+                        //ProductContainer.Controls.Add(productControl);
+                        ProductContainer.Controls.Add(productControl);
+
+                    }
+                }
+                rdr.Close();
+            }
+        }
+
+        protected void Add_to_Cart(object sender, EventArgs e)
+        {
+            // alert the user that the product has been added to the cart
+            Response.Write("<script>alert('Product has been added to the cart!');</script>");
+
+        }
+    }
+}
