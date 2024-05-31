3ff74e14ce808a193e07f23c2c5e9012299b2170
Vishvas Lathiya <21bmiit127@gmail.com>
2024-05-31 20:40:18 +0530

    Update and rename About_Us..aspx.cs to About_Us.aspx.cs

--- Customer/About_Us.aspx.cs
+++ Customer/About_Us.aspx.cs
@@ -3,6 +3,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
     <br />
 
     <div class="bg-gray-100 py-16">
